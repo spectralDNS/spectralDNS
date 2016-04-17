47ad5afd216b56dc2404342128d0be01af071dd2
Nathanael Schilling <nathanaelschilling@gmx.net>
2016-04-17 18:43:30 +0300

    Fix a bug

--- spectralDNS/solvers/spectralinit.py
+++ spectralDNS/solvers/spectralinit.py
@@ -3,22 +3,20 @@ __date__ = "2014-11-07"
 __copyright__ = "Copyright (C) 2014-2016 " + __author__
 __license__  = "GNU Lesser GPL version 3 or any later version"
 
-from mpi4py import MPI
-from spectralDNS import config
 import sys, cProfile
-from numpy import *
-from mpiFFT4py import slab_FFT, pencil_FFT, line_FFT
+
 from spectralDNS.utilities import *
 from spectralDNS.h5io import *
-from spectralDNS.optimization import *
 
 # Import problem specific methods and solver methods specific to either slab or pencil decomposition
-from spectralDNS.mesh import *
 from spectralDNS.maths import *
 
 import spectralDNS.context
 
+#TODO:Figure out what the lines below do
+"""
 def set_source(Source, **kwargs):
     Source[:] = 0
     return Source
+    """
 
