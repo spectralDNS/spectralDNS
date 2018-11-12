6a41212d56b47daa1463814d0f868e337a4f0f3a
Mikael Mortensen <mikaem@math.uio.no>
2018-11-12 15:10:53 +0100

    Adding test for KMMr, channel solver with inhomogen direction last

---
+++ tests/test_channelz.py
@@ -0,0 +1,45 @@
+import pytest
+from six.moves import reload_module
+from spectralDNS import config, get_solver, solve
+from OrrSommerfeldr import initialize, regression_test, set_Source, pi
+
+@pytest.fixture(params=('KMMr',))
+def sol(request):
+    """Check for uniform and non-uniform cube"""
+    return request.param
+
+def test_channel(sol):
+    config.update(
+        {
+            'Re': 8000.,
+            'nu': 1./8000.,              # Viscosity
+            'dt': 0.001,                 # Time step
+            'T': 0.01,                   # End time
+            'L': [2*pi, pi, 2],
+            'M': [2, 5, 7],
+            'eps': 1e-7
+        }, "channel"
+    )
+
+    solver = get_solver(regression_test=regression_test,
+                        mesh="channel",
+                        parse_args=[sol])
+    context = solver.get_context()
+    initialize(solver, context)
+    set_Source(**context)
+    solve(solver, context)
+
+    config.params.dealias = '3/2-rule'
+    config.params.optimization = 'cython'
+    reload_module(solver) # Need to reload to enable optimization
+    initialize(solver, context)
+    solve(solver, context)
+
+    config.params.dealias_cheb = True
+    config.params.checkpoint = 5
+    config.params.write_result = 2
+    initialize(solver, context)
+    solve(solver, context)
+
+if __name__ == '__main__':
+    test_channel('KMMr')
