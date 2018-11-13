b484fa524ed4c6931c1017d96ff9508dc35f13fe
Mikael Mortensen <mikaem@math.uio.no>
2018-11-13 13:45:30 +0100

    Removed another test no longer needed

--- tests/test_shentransforms.py
+++ tests/test_shentransforms.py
@@ -77,45 +77,6 @@ def test_Mult_Div():
 
 #test_Mult_Div()
 
-
-@pytest.mark.parametrize('quad', quads)
-def test_Mult_CTD(quad):
-    SD = ShenDirichletBasis(N, quad=quad)
-    SD.plan(N, 0, np.complex, {})
-    C = inner_product((SD.CT, 0), (SD, 1))
-    B = inner_product((SD.CT, 0), (SD.CT, 0))
-
-    vk = np.random.randn((N))+np.random.randn((N))*1j
-    wk = np.random.randn((N))+np.random.randn((N))*1j
-
-    bv = np.zeros(N, dtype=np.complex)
-    bw = np.zeros(N, dtype=np.complex)
-    vk0 = np.zeros(N, dtype=np.complex)
-    wk0 = np.zeros(N, dtype=np.complex)
-    cv = np.zeros(N, dtype=np.complex)
-    cw = np.zeros(N, dtype=np.complex)
-
-    vk0 = SD.forward(vk, vk0)
-    vk = SD.backward(vk0, vk)
-    vk0 = SD.forward(vk, vk0)
-    wk0 = SD.forward(wk, wk0)
-    wk = SD.backward(wk0, wk)
-    wk0 = SD.forward(wk, wk0)
-
-    LUsolve.Mult_CTD_1D(N, vk0, wk0, bv, bw)
-
-    cv = np.zeros_like(vk0)
-    cw = np.zeros_like(wk0)
-    cv = C.matvec(vk0, cv)
-    cw = C.matvec(wk0, cw)
-    cv /= B[0]
-    cw /= B[0]
-
-    assert np.allclose(cv, bv)
-    assert np.allclose(cw, bw)
-
-#test_Mult_CTD("GC")
-
 @pytest.mark.parametrize('quad', quads)
 def test_Mult_CTD_3D(quad):
     SD = ShenDirichletBasis(N, quad=quad)
