63987673e531cce35edba3493a23f6324e565e3f
Mikael Mortensen <mikaem@math.uio.no>
2022-06-29 22:12:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
               'Topic :: Scientific/Engineering :: Mathematics',
               'Topic :: Software Development :: Libraries :: Python Modules',
               ],
-          install_requires=['numpy', 'shenfun>=3.0.0', 'cython', 'mpi4py-fft', 'mpi4py'],
+          install_requires=['numpy', 'shenfun>=4.0.2', 'cython', 'mpi4py-fft', 'mpi4py'],
           setup_requires=['numpy', 'cython>=0.25', 'setuptools>=18.0'],
           packages=["spectralDNS",
                     "spectralDNS.h5io",
